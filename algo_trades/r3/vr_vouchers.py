f7d2c0f7560122bddc1b6b0ebd46ba99559d50df
shreyask05 <skonanki05@gmail.com>
2025-04-15 23:33:23 -0700

    everything put together for now

--- algo_trades/r3/vr_vouchers.py
+++ algo_trades/r3/vr_vouchers.py
@@ -133,9 +133,278 @@ class Logger:
                 hi = mid - 1
 
         return out
-
-
 logger = Logger()
+
+LIMITS = {
+    'RAINFOREST_RESIN' : 50,
+    'KELP' : 50,
+    'SQUID_INK' : 50,
+    'CROISSANTS' : 250,
+    'JAMS' : 350,
+    'DJEMBES' : 60,
+    'PICNIC_BASKET1' : 60,
+    'PICNIC_BASKET2' :100,
+    'VOLCANIC_ROCK' : 400,
+}
+
+PROD_TR_AR = {
+    'RAINFOREST_RESIN' : 10000,
+    'KELP' : 2000,
+    'SQUID_INK' : -1,
+    'CROISSANTS' : -1,
+    'JAMS' : -1,
+    'DJEMBES' : -1,
+    'PICNIC_BASKET1' : -1,
+    'PICNIC_BASKET2' : -1,
+    'VOLCANIC_ROCK' : 10280
+}
+
+class ProductTrader:
+
+    def __init__(self, state: TradingState, traderDict: Dict, name: str, ppw: int):
+
+        # declare ALL variables here first
+        # yer
+        self.name = name
+        self.od = state.order_depths.get(self.name, {})
+        self.trader_data: Dict = traderDict.get(self.name, {})
+
+        # price vars
+        self.prev_prices = []
+        self.prev_price_window = ppw if ppw != 0 else 101
+        self.best_buy = 0
+        self.best_sell = 0
+        self.midprice = 0
+        self.trade_around = PROD_TR_AR[self.name]
+        self.gap_trigger = 3
+
+        # volume vars
+        self.curr_pos = state.position.get(self.name, 0)
+        self.pos_lim = LIMITS[self.name]
+        self.buying_power = self.pos_lim - self.curr_pos
+        self.selling_power = self.pos_lim + self.curr_pos
+        self.market_take_buy_vol = 10
+        self.market_take_sell_vol = 10
+        self.market_make_buy_vol = 10
+        self.market_make_sell_vol = 10
+
+        ## probably don't need these
+        # self.buyVol_this_timestamp = 0
+        # self.sellVol_this_timestamp = 0
+        ################################
+
+        # call calculations functions
+        (self.best_buy, self.midprice, self.best_sell) = self.calc_vwaps()
+        self.gap = self.best_sell - self.best_buy if self.best_buy and self.best_sell else -1
+
+        ######################################
+        # Pull out whatever from trader_data #
+        ######################################
+        self.get_prev_prices()
+        self.smooth_price = self.savgol_filter_manual(self.prev_prices)
+
+        ########################
+        # SMOOOOOTTTH OPERATOR #
+        ########################
+
+    def savgol_filter_manual(self, a: list, window_size: int = 101, poly_order: int = 2) -> float:
+        """
+        Applies Savitzky-Golay filtering to the input list of midprices.
+
+        Parameters:
+            y (list): List of midprices, length must equal window_size.
+            window_size (int): Must be odd. The number of points to consider in the window.
+            poly_order (int): The order of the polynomial to fit (e.g., 2 = quadratic).
+
+        Returns:
+            float: The smoothed value at the center of the window.
+        """
+        "Length of input must match window_size"
+        if len(a) < self.prev_price_window:
+            return self.midprice
+
+        y = a[-self.prev_price_window:]
+        assert window_size % 2 == 1, "Window size must be odd"
+
+        half = window_size // 2
+        x = np.arange(-half, half + 1)  # e.g., [-50, ..., 0, ..., +50]
+        X = np.vander(x, poly_order + 1, increasing=True)  # Vandermonde matrix
+        X_pinv = np.linalg.pinv(X)  # Pseudo-inverse for least squares
+
+        coeffs = X_pinv @ np.array(y)
+        smoothed_val = coeffs[0]  # Value at center of polynomial (x=0)
+
+        return smoothed_val
+
+    def calc_vwaps(self) -> tuple[int, int, int]:
+        """
+        Returns the (best_buy, midprice, best_sell) using volume weighted average products
+
+        does vwap on buy orders --> best buy
+        does vwap on sell orders --> best sell
+        avg buy/sell -> mid
+        """
+        buy_sum = 0
+        buy_vol = 0
+        sell_sum = 0
+        sell_vol = 0
+
+        if (self.od != {} and len(self.od.buy_orders)):
+            for key, value in self.od.buy_orders.items():
+                buy_sum += key * value
+                buy_vol += value
+            best_buy = int(math.ceil(buy_sum / buy_vol))
+        else:
+            best_buy = 0
+
+        if (self.od != {} and len(self.od.sell_orders)):
+            for key, value in self.od.sell_orders.items():
+                sell_sum += key * value
+                sell_vol += value
+            best_sell = int(math.floor(sell_sum / sell_vol))
+        else:
+            best_sell = 0
+
+        # TODO: Revaluate this
+        midprice = (best_buy + best_sell) // 2 if best_buy and best_sell else -1
+
+        return (best_buy, midprice, best_sell)
+
+    # this returns the last prev_price_window prices including this timestamp
+    def get_prev_prices(self) -> None:
+        prev_prices = self.trader_data.get('prev_prices', [])
+
+        if (len(prev_prices) > self.prev_price_window):
+            prev_prices.pop(0)
+
+        prev_prices.append(self.midprice)
+        self.prev_prices = prev_prices
+        self.trader_data['prev_prices'] = prev_prices
+
+    def balance(self, result: Dict[str, List[Order]]):
+        orders: List[Order] = []
+        if (self.curr_pos != 0):
+
+            ##TODO: buy/sell at the farthest price from trade-around for balancing
+
+            if (self.curr_pos < 0):
+                orders.append(Order(self.name, self.best_buy + 1, -self.curr_pos))
+                self.buying_power += self.curr_pos
+            else:
+                orders.append(Order(self.name, self.best_sell - 1, -self.curr_pos))
+                self.selling_power += self.curr_pos
+
+        if self.name in result:
+            result[self.name].extend(orders)
+        else:
+            result[self.name] = orders
+
+    # ABSOLUTE VALUES OF VOLUME
+    def short(self, price: int, abs_quantity: int, result: Dict[str, List[Order]]):
+        if self.name in result:
+            result[self.name].extend([Order(self.name, price, -abs_quantity)])
+        else:
+            result[self.name] = [Order(self.name, price, -abs_quantity)]
+
+    def long(self, price: int, abs_quantity: int, result: Dict[str, List[Order]]):
+        if self.name in result:
+            result[self.name].extend([Order(self.name, price, abs_quantity)])
+        else:
+            result[self.name] = [Order(self.name, price, abs_quantity)]
+
+    def market_take(self, result: Dict[str, List[Order]]):
+        orders: List[Order] = []
+
+        # TODO: IMPLEMENT SMARTER ORDER VOLUME CHOICE
+        i = 0
+        for key, val in self.od.buy_orders.items():
+            i += 1
+            if (i > 3):
+                break
+            if (key > self.trade_around):
+                orders.append(Order(self.name, key, -self.market_take_sell_vol))
+                self.selling_power -= self.market_take_sell_vol
+
+        j = 0
+        for key, val in self.od.sell_orders.items():
+            j += 1
+            if (j > 3):
+                break
+            if (key < self.trade_around):
+                orders.append(Order(self.name, key, self.market_take_buy_vol))
+                self.buying_power -= self.market_take_buy_vol
+
+        if self.name in result:
+            result[self.name].extend(orders)
+        else:
+            result[self.name] = orders
+
+    def market_make(self, result: Dict[str, List[Order]]):
+        orders: List[Order] = []
+
+        # TODO: IMPLEMENT SMARTER ORDER VOLUME CHOICE
+
+        for backoff in range(1, 20):
+
+            bv_ = int(self.market_make_buy_vol / (backoff))
+            sv_ = int(self.market_make_sell_vol / (backoff))
+
+            logger.print(f'bv_ {bv_} > self.buying_power {self.buying_power}')
+            logger.print(f'sv_ {sv_} > self.buying_power {self.buying_power}')
+
+            if bv_ > self.buying_power or sv_ > self.selling_power:
+                logger.print(f"backoff {backoff}")
+                continue
+
+            logger.print(f'sv_ {self.gap} > self.buying_power {self.gap_trigger}')
+
+            if self.gap >= self.gap_trigger:
+                logger.print(f"backoff {backoff}")
+
+                orders.append(Order(self.name, self.best_buy + 1, bv_))
+                orders.append(Order(self.name, self.best_sell - 1, -sv_))
+                break
+
+        if self.name in result:
+            result[self.name].extend(orders)
+        else:
+            result[self.name] = orders
+
+    def balance(self, result: Dict[str, List[Order]]):
+        orders: List[Order] = []
+        if (self.curr_pos != 0):
+
+            ##TODO: buy/sell at the farthest price from trade-around for balancing
+
+            if (self.curr_pos < 0):
+                orders.append(Order(self.name, self.best_buy + 1, -self.curr_pos))
+                # confusing, yes, but position < 0, and we are BUYING so we want to reduce our buying power
+                self.buying_power += self.curr_pos
+            else:
+                orders.append(Order(self.name, self.best_sell - 1, -self.curr_pos))
+                # confusing, yes, but position > 0, and we are SELLING so we want to reduce our selling power
+                self.selling_power -= self.curr_pos
+
+        if self.name in result:
+            result[self.name].extend(orders)
+        else:
+            result[self.name] = orders
+
+
+class VRTrader(ProductTrader):
+    def __init__(self, state: TradingState, traderData: Dict, result:Dict[Symbol, List[Order]], ppw:int):
+        super().__init__(state, traderData, 'VOLCANIC_ROCK', ppw)
+
+        # GRID SEARCHED
+        self.mm_vol_r = 0.5
+        self.mt_bv = 15
+        self.mt_sv = 15
+        self.gap_trigger = 4
+        self.best_delta = 1
+
+        # NOT GRID SEARCHED (but necessary for product trader)
+        self.market_make_buy_vol = 120
+        self.market_make_sell_vol = 120
 
 
 class OptionsTrader:
@@ -174,7 +443,6 @@ class OptionsTrader:
 
     def compute_m_t(self, K, S, T):
         return np.log(K / S) / np.sqrt(T)
-
 
 
 class VR_VoucherTrader:
@@ -306,8 +574,6 @@ class VR_VoucherTrader:
         return (a_num / det, b_num / det, c_num / det)
 
 
-
-
 class Trader:
     def run(self, state: TradingState):
         traderData = json.loads(state.traderData) if state.traderData else {}
@@ -323,6 +589,11 @@ class Trader:
 
         for name, strike in option_configs.items():
             VR_VoucherTrader(name, strike, state, traderData).trade(result)
+
+        vr = VRTrader(state, traderData, result, 101)
+        #vr.balance(result)
+        #vr.market_make(result)
+        vr.market_take(result)
 
         logger.flush(state, result, 0, json.dumps(traderData))
         return result, 0, json.dumps(traderData)
